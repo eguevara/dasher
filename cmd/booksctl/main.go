3dbdae916919cfeb767113aa41ee97d055a84b09
Erick Guevara <erick@motorola.com>
2017-10-05 21:09:17 -0500

    Adding booksctl to list books and quotes from cmdline

---
+++ cmd/booksctl/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
+	"github.com/eguevara/dasher/api"
+	"github.com/eguevara/dasher/config"
+	"github.com/eguevara/dasher/pkg/feelinglucky"
+	books "github.com/eguevara/go-books"
+)
+
+func main() {
+
+	var (
+		flagConfigPath = flag.String("config-file", "./config.json", "application configuration file")
+		flagBookID     = flag.String("book-id", "", "the id of the book")
+		flagShowBooks  = flag.Bool("show-books", false, "show all books for a shelf")
+		flagLucky      = flag.Bool("feeling-lucky", false, "feeling lucky")
+	)
+
+	flag.Parse()
+
+	cfg := buildConfigFromFie(flagConfigPath)
+
+	if *flagLucky == true {
+		opts := &feelinglucky.Request{
+			Config: cfg,
+			Shelf:  "1",
+		}
+
+		svc := feelinglucky.NewService(opts)
+
+		quote, err := svc.Annotation()
+		if err != nil {
+			log.Fatalf("error in getting annotation: %v", err)
+		}
+		fmt.Printf("\"%v\n\n\n%v\n", *quote.SelectedText, quote.Title)
+	}
+
+	if *flagBookID != "" || *flagShowBooks {
+		svc := api.NewBooksHandler(cfg)
+
+		if *flagShowBooks {
+			opts := &books.VolumesListOptions{
+				MaxResults: 100,
+			}
+
+			books, _, err := svc.Client.Volumes.List("1", opts)
+			if err != nil {
+				fmt.Printf("could not find any books in this shelf: %v", err)
+			}
+
+			for _, book := range books {
+				fmt.Printf("Book: %v, ID: %v\n\n", *book.Info.Title, *book.ID)
+			}
+
+			return
+		}
+		opts := &books.AnnotationsListOptions{
+			VolumeID:       *flagBookID,
+			ContentVersion: "full-1.0.0",
+			MaxResults:     40,
+			Fields:         "items(layerId,selectedText,volumeId),totalItems",
+		}
+
+		list, _, err := svc.Client.Annotations.List(opts)
+		if err != nil {
+			log.Fatalf("error in list(): %v ", err)
+		}
+
+		fmt.Printf("Count: %v\n\n", len(list))
+
+		for _, quote := range list {
+			fmt.Printf("...%v...\n\n", *quote.SelectedText)
+		}
+
+	}
+
+}
+
+func buildConfigFromFie(file *string) *config.AppConfig {
+
+	configFile := filepath.Join(*file)
+	data, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Fatalf("DASHER error reading config.json: %v", err)
+	}
+
+	config := &config.AppConfig{}
+	if err := json.Unmarshal(data, &config); err != nil {
+		log.Fatalf("DASHER setting up app configuration: %v", err)
+	}
+
+	return config
+}
---
+++ pkg/feelinglucky/main.go
@@ -0,0 +1,115 @@
+package feelinglucky
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/eguevara/dasher/common"
+	"github.com/eguevara/dasher/config"
+	books "github.com/eguevara/go-books"
+)
+
+// LuckyService defines the behavior required by types that implement a new
+// feeling lucky type.
+type LuckyService interface {
+	Annotation() (*LuckyAnnotation, error)
+}
+
+// Request stores options needed to make feelinglucky requests.
+type Request struct {
+	// Config holds the oauth credential used by the api.
+	Config *config.AppConfig
+
+	// Shelf is the bookshelf to look for volumes/annotations.
+	Shelf string
+}
+
+type lucky struct {
+	// Options holds all stateful fields added to the request.
+	Options *Request
+
+	// Client holds the book service client to communicate to the books api.
+	Client *books.Client
+
+	Title string
+}
+
+// LuckyAnnotation holds annotation information to send as a response.
+type LuckyAnnotation struct {
+	// A wrapper field to hold Annotation fields.
+	*books.Annotation
+
+	// The title of the book.
+	Title string
+}
+
+// NewService returns a new instance of the lucky type.
+func NewService(opts *Request) LuckyService {
+	client := common.GetOAuthClient(opts.Config.BooksOAuth)
+	booksClient, err := books.New(client)
+	if err != nil {
+		log.Fatal("error on creating Books client")
+	}
+	return &lucky{
+		Options: opts,
+		Client:  booksClient,
+	}
+}
+
+// Validates that we implement the LuckyService interface.
+var _ LuckyService = &lucky{}
+
+// Annotation returns a random luckyannotation of any of the volumes available.
+func (l *lucky) Annotation() (*LuckyAnnotation, error) {
+	book, err := l.book()
+	if err != nil {
+		log.Fatalf("could not find volume: %v", err)
+	}
+
+	opts := &books.AnnotationsListOptions{
+		VolumeID:       *book.ID,
+		ContentVersion: *book.Info.ContentVersion,
+		MaxResults:     40,
+		Fields:         "items(layerId,selectedText,volumeId),totalItems",
+	}
+
+	list, _, err := l.Client.Annotations.List(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(list) == 0 {
+		return nil, fmt.Errorf("no annotations found: %v", l.Title)
+	}
+
+	generator := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	randomIndex := generator.Intn(len(list))
+
+	annotation := &LuckyAnnotation{
+		Title:      l.Title,
+		Annotation: &list[randomIndex],
+	}
+	return annotation, nil
+}
+
+// Book returns a random volume from the bookshelves avaialble.
+func (l *lucky) book() (*books.Volume, error) {
+	opts := &books.VolumesListOptions{
+		Fields:     "items(id,volumeInfo(contentVersion,title)),totalItems",
+		MaxResults: 40,
+	}
+
+	volumes, _, err := l.Client.Volumes.List(l.Options.Shelf, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	generator := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	randomIndex := generator.Intn(len(volumes))
+
+	l.Title = *volumes[randomIndex].Info.Title
+
+	return &volumes[randomIndex], nil
+}
