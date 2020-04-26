e9c0d90107f55fe7dd2bfc6d9db079bf9e08b3df
Erick Guevara <eguevara@github.com>
2020-04-25 21:40:04 -0700

    Adding sync notes and sync books

--- cmd/booksctl/main.go
+++ cmd/booksctl/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/google/go-github/github"
 
@@ -34,6 +35,8 @@ func main() {
 		flagBookID          = flag.String("book-id", "", "the id of the book")
 		flagShowBooks       = flag.Bool("show-books", false, "show all books for a shelf")
 		flagGitHubShowBooks = flag.Bool("show-ghbooks", false, "show all github books")
+		flagSyncGitHubBook  = flag.Bool("sync-books", false, "Syncs google books to github issues")
+		flagSyncGitHubNotes = flag.String("sync-notes", "", "Syncs google book notes to github comments")
 		flagShowBookShelf   = flag.Bool("show-bookshelf", false, "show all bookshelfs")
 		flagLucky           = flag.Bool("feeling-lucky", false, "feeling lucky")
 	)
@@ -46,30 +49,125 @@ func main() {
 		opts := &githubbooks.Request{
 			Config: cfg,
 		}
+
 		githubService := githubbooks.NewService(opts)
 
+		resp, err := githubService.List()
+		if err != nil {
+			log.Fatalf("error in getting github books: %v", err)
+		}
+
+		for _, b := range resp.Issues {
+			fmt.Printf("Adding book: %v\n", *b.Title)
+		}
+
+	}
+
+	if *flagSyncGitHubBook {
+		opts := &githubbooks.Request{
+			Config: cfg,
+		}
+		githubService := githubbooks.NewService(opts)
 		googleBookService := api.NewBooksHandler(cfg)
 
-		books, _, err := googleBookService.Client.Volumes.List("1", nil)
+		volumeOpts := &books.VolumesListOptions{
+			MaxResults: 100,
+		}
+		books, _, err := googleBookService.Client.Volumes.List("1", volumeOpts)
 		if err != nil {
 			fmt.Printf("could not find any books in this shelf: %v", err)
 		}
 
 		for _, book := range books {
-			fmt.Printf("Book: %v, ID: %v\n\n", *book.Info.Title, *book.ID)
+			_, found, err := githubService.BookExists(*book.Info.Title)
+			if err != nil || found {
+				log.Printf("Skipping book: %v", err)
+				continue
+			}
 
 			bookOpt := &github.IssueRequest{
-				Title:  book.Info.Title,
+				Title: book.Info.Title,
+
+				//TODO: make this configurable.
 				Labels: &[]string{"google-books"},
 			}
+
 			ghIssue, err := githubService.AddBook(bookOpt)
 
 			if err != nil {
 				log.Fatalf("error in List(): %v", err)
 			}
+			fmt.Printf("Adding book: %v, %v - %v\n", *book.Info.Title, bookOpt, *ghIssue.Title)
 
-			fmt.Printf("Adding book: %v", ghIssue.Title)
+		}
+		return
+	}
+	if *flagSyncGitHubNotes != "" {
+		opts := &githubbooks.Request{
+			Config: cfg,
+		}
+		githubService := githubbooks.NewService(opts)
+		googleBookService := api.NewBooksHandler(cfg)
 
+		volumeOpts := &books.VolumesListOptions{
+			MaxResults: 100,
+		}
+		resp, _, err := googleBookService.Client.Volumes.List("1", volumeOpts)
+		if err != nil {
+			fmt.Printf("could not find any books in this shelf: %v", err)
+		}
+
+		for _, book := range resp {
+			if *flagSyncGitHubNotes != *book.ID {
+				continue
+			}
+			githubBook, notFound, err := githubService.BookExists(*book.Info.Title)
+			if err != nil || !notFound {
+				log.Printf("Skipping: %v", err)
+				continue
+			}
+
+			opts := &books.AnnotationsListOptions{
+				VolumeID:       *book.ID,
+				ContentVersion: "full-1.0.0",
+				MaxResults:     40,
+				Fields:         "items(layerId,selectedText,volumeId,pageIds),totalItems,nextPageToken",
+			}
+
+			totalNotes := 0
+			for {
+				list, resp, err := googleBookService.Client.Annotations.List(opts)
+				if err != nil {
+					log.Fatalf("error in list(): %v ", err)
+				}
+
+				for _, note := range list {
+					if *note.SelectedText == "" {
+						totalNotes--
+						continue
+					}
+
+					ghComment := buildComment(note)
+					_, err := githubService.AddNote(*githubBook.Number, ghComment)
+					if err != nil {
+						log.Fatalf("error: %v", err)
+					}
+
+				}
+
+				totalNotes += len(list)
+
+				// If there is no more nextPage tokens, then you are done!
+				if resp.NextPageToken == "" {
+					break
+				}
+
+				// Update the token to the next page to fetch the rest.
+				opts.PageToken = resp.NextPageToken
+			}
+
+			//fmt.Printf(resp.s)
+			fmt.Printf("Count: %v\n\n", totalNotes)
 		}
 		return
 	}
@@ -142,6 +240,19 @@ func main() {
 
 	}
 
+}
+
+// buildComment takes a note and builds a comment used as an annotation.
+// Returns a String
+func buildComment(note books.Annotation) string {
+	pages := strings.Join(note.PageIds, ", ")
+	pages = strings.Replace(pages, "PA", "page: ", -1)
+	pages = strings.Replace(pages, "PT", "page: ", -1)
+	pages = strings.Replace(pages, "PR", "page: ", -1)
+
+	comment := fmt.Sprintf("%v\n%v", pages, *note.SelectedText)
+
+	return comment
 }
 
 func buildConfigFromFie(file *string) *config.AppConfig {
--- pkg/githubbooks/main.go
+++ pkg/githubbooks/main.go
@@ -2,6 +2,7 @@ package githubbooks
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/go-github/github"
 
@@ -9,13 +10,22 @@ import (
 	"github.com/eguevara/dasher/config"
 )
 
+// BookService is an interface to represents a book model.
 type BookService interface {
-	// AddBook creates a new github issue on a repo masked as a book.
+	// AddBook creates a new book.
 	AddBook(request *github.IssueRequest) (*github.Issue, error)
 
-	List(request *github.IssueListByRepoOptions) (*BooksResponse, error)
+	// BookExists returns the book found if it exists.
+	BookExists(string) (*github.Issue, bool, error)
+
+	// List will list out all books found.
+	List() (*BooksResponse, error)
+
+	// AddNote adds a book annotation.
+	AddNote(int, string) (*github.IssueComment, error)
 }
 
+// BooksResponse represents what the book service returns.
 type BooksResponse struct {
 	Issues []*github.Issue
 }
@@ -49,13 +59,27 @@ func NewService(opts *Request) BookService {
 var _ BookService = &githubbook{}
 
 // AddBook will add a new issue to an existing GitHub repo to mask as a Book.
+func (b *githubbook) AddNote(issue int, body string) (*github.IssueComment, error) {
+	issueComment := &github.IssueComment{Body: &body}
+	comment, _, err := b.Client.Issues.CreateComment(
+		context.Background(),
+		b.Options.Config.GitHub.Owner,
+		b.Options.Config.GitHub.Repo,
+		issue,
+		issueComment)
+
+	if err != nil {
+		return nil, err
+	}
+	return comment, nil
+}
+
+// AddBook will add a new issue to an existing GitHub repo to mask as a Book.
 func (b *githubbook) AddBook(opt *github.IssueRequest) (*github.Issue, error) {
 	issue, _, err := b.Client.Issues.Create(
 		context.Background(),
 		b.Options.Config.GitHub.Owner,
-		b.Options.Config.GitHub.Repo,
-		opt,
-	)
+		b.Options.Config.GitHub.Repo, opt)
 
 	if err != nil {
 		return nil, err
@@ -64,14 +88,12 @@ func (b *githubbook) AddBook(opt *github.IssueRequest) (*github.Issue, error) {
 }
 
 // List returns a list of books from github masked as issues.
-func (b *githubbook) List(opt *github.IssueListByRepoOptions) (*BooksResponse, error) {
+func (b *githubbook) List() (*BooksResponse, error) {
 	ghIssues, _, err := b.Client.Issues.ListByRepo(
 		context.Background(),
 		b.Options.Config.GitHub.Owner,
 		b.Options.Config.GitHub.Repo,
-		opt,
-	)
-
+		nil)
 	if err != nil {
 		return nil, err
 	}
@@ -81,4 +103,24 @@ func (b *githubbook) List(opt *github.IssueListByRepoOptions) (*BooksResponse, e
 	}
 
 	return issues, nil
+}
+
+// bookExist checks whether the book already exists in the repo as an issue.
+// Uses the search api to do a search on the :title and is:issue qualifiers.
+//
+// Returns The github.Issue and boolean.
+func (b *githubbook) BookExists(bookTitle string) (*github.Issue, bool, error) {
+
+	query := fmt.Sprintf("repo:%v/%v is:issue \"%v\" in:title", b.Options.Config.GitHub.Owner, b.Options.Config.GitHub.Repo, bookTitle)
+	result, _, err := b.Client.Search.Issues(context.Background(), query, nil)
+
+	if err != nil {
+		return nil, true, err
+	}
+
+	if len(result.Issues) > 0 {
+		return result.Issues[0], true, nil
+	}
+
+	return nil, false, nil
 }
